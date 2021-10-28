4c1f6687281f0f1d771588685cb5df4856773651
sne2047 <sne2047@rit.edu>
2021-10-28 16:11:31 -0400

    Finished domo B

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,5 +1,49 @@
+const models = require('../models');
+
+const { Domo } = models;
+
 const makerPage = (req, res) => {
-  res.render('app');
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+
+    return res.render('app', { domos: docs });
+  });
+};
+
+const makeDomo = (req, res) => {
+  // check that we actually have the required info
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'RAWR! Both name and age are required!' });
+  }
+
+  // data for new domo
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  // make our new domo
+  const newDomo = new Domo.DomoModel(domoData);
+
+  // set up the promise & add the appropriate functions
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({ redirect: '/maker' }))
+    .catch((err) => {
+      console.log(err);
+      if (err.code === 11000) {
+        return res.status(400).json({ error: 'Domo already exists' });
+      }
+      return res.status(400).json({ error: 'An error occured' });
+    });
+
+  // and return
+  return domoPromise;
 };
 
 module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
