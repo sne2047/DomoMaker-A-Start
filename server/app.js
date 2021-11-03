0b090d45a63c640099635d412caf4ae482a3d314
sne2047 <sne2047@rit.edu>
2021-11-03 11:52:36 -0400

    finished domo c

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -19,6 +23,24 @@ mongoose.connect(dbURL, (err) => {
   }
 });
 
+//redis stuff
+let redisURL = {
+  hostname: 'redis-17320.c244.us-east-1-2.ec2.cloud.redislabs.com',
+  port: '17320'
+};
+
+let redisPASS = '4mutNNzZzMP3CUpAKuaqcSlHguJj1qNL';
+if(process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPass] = redisURL.auth.split(':');
+}
+let redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
+});
+
+
 // bring in our router
 const router = require('./router.js');
 
@@ -26,20 +48,34 @@ const app = express();// here's our app
 // now gotta get the app set up
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+//csrf needs to be here, after the cookie parser and before the router
+app.use(csrf());
+app.use((err,req,res,next)=> {
+  if(err.code !== 'EBADCSRFTOKEN') return next (err);
+  console.log('Missing CSRF token');
+  return false;
+});
 
 // okay now hook up router
 router(app);
